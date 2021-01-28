f8a0d0ebc8055b95798ab57d9694d4fbe6ac453d
chukwuebuka <emysly12@gmail.com>
2021-01-28 13:08:29 +0100

    modified server js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import 'colors'
 
 import validationRoutes from './routes/validationRoutes.js'
 import { errorHandler, notFound } from './middleware/errorHandler.js'
@@ -19,7 +18,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>
-  console.log(
-    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
